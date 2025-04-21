8dc8f32c597c4908278b88136b13cb204b0bd2ed
takenokos <takenokos0916@outlook.com>
2025-04-21 14:10:29 +0800

    update: complete tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://takenokos.netlify.app",
+  integrations: [preact()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
